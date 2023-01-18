7a44392686a0a797d696169c694a080eaaa24721
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-18 02:52:48 +0000

    add properties add output input lines

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
+  // lab4.cpp 
+// Determine how many brownies will fit in specific baking pans
+// Marc Jacques mjacque5@asu.edu 
+// Write a preprocessor directive below which enables you to perform I/O 
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+    // Below, declare all the variables you will use and make certain to give each variable a descriptive name which explains what the variable represents. 
+  int length, width, small_brownies, large_brownies;
+    
+  // Write a statement to output a prompt for the information the program needs to get from the user. 
+  cout << "Enter length of baking pan" << endl;
+  cin >> length;
+  cout << "Enter length of baking pan" << endl;
+  cin >> width;
+ 
+  // Write a statement here to read the input from the user and save it in a variable. 
+ 
+    // Write statements below which will perform the required computations and 
+    // assign the results to variables. 
+ 
+    // Write statements below which will output the requested information. 
+ 
+  return 0;
 }
