6b7827d5172b1b87569f7ae8bc8cbdde48fe113b
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-18 22:52:17 +0000

    add input questions add computations add output statements

--- .ccls-cache/@home@runner@lab4/main.cpp
+++ .ccls-cache/@home@runner@lab4/main.cpp
@@ -11,17 +11,20 @@ int main()
   int length, width, small_brownies, large_brownies;
     
   // Write a statement to output a prompt for the information the program needs to get from the user. 
-  cout << "Enter length of baking pan" << endl;
+  cout << "How long is the pan?" << endl;
   cin >> length;
-  cout << "Enter length of baking pan" << endl;
+  cout << "How wide is the pan?" << endl;
   cin >> width;
  
   // Write a statement here to read the input from the user and save it in a variable. 
  
     // Write statements below which will perform the required computations and 
     // assign the results to variables. 
+  small_brownies = length * width;
+  large_brownies = small_brownies/4;
  
-    // Write statements below which will output the requested information. 
+    // Write statements below which will output the requested information.
+  cout << "A " << length << "X" << width << " inch pan can hold " << small_brownies << " small brownies or " << large_brownies << " large brownies."
  
   return 0;
 }
